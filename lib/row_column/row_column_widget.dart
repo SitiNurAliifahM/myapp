e73ac42bbe251d0b2efcfb6af9bd5fc7baa6e4de
sitinuraliifah587 <sitinuraliifah587@gmail.com>
2024-07-02 05:01:53 +0000

    Row Column

--- lib/row_column/row_column_widget.dart
+++ lib/row_column/row_column_widget.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:604203841.
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2151198751.
 class BelajarRowColumn extends StatelessWidget {
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:3667732809.
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            child: Text('Text 1'),
-          ),
-          Container(
-            child: Text('Text 2'),
-          ),
-          Container(
-            child: Text('Text 3'),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Text 1'),
+            Text('Text 2'),
+            Text('Text 3'),
+          ],
+        ),
+        Text('Text'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Text 1'),
+            Text('Text 2'),
+            Text('Text 3'),
+           ],
+        ),
+      ],
     );
   }
 }
